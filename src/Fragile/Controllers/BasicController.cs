2b37e1c4fab5dec7725098a3a5207aca5c07cb3e
Gage Orsburn <gageorsburn@live.com>
2016-02-07 00:43:25 -0600

    Added files that were untracked.

---
+++ src/Fragile/Controllers/BasicController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc;
+
+using Fragile.Services;
+using Fragile.Models;
+
+namespace Fragile.Controllers
+{
+    public class BasicController : Controller
+    {
+        public ApplicationDbContext DbContext
+        {
+            get;
+        }
+
+        public AuthenticationService AuthenticationService
+        {
+            get;
+        }
+
+        public BasicController(ApplicationDbContext dbContext, AuthenticationService authenticationService)
+        {
+            this.DbContext = dbContext;
+            this.AuthenticationService = authenticationService;
+        }
+    }
+}
